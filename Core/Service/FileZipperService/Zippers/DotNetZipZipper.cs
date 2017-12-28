0f3b6220efa3eb745ed56695cc91dc17c6a6eb50
fmoutinho <fernandom@gmail.com>
2017-12-28 17:41:17 -0200

    FileZipperService - Remove mais de um arquivo splittado que foi zippado

--- Core/Service/FileZipperService/Zippers/DotNetZipZipper.cs
+++ Core/Service/FileZipperService/Zippers/DotNetZipZipper.cs
@@ -2,7 +2,10 @@
 using Core.Service.FileZipperService.Interfaces;
 using Core.Util;
 using Ionic.Zip;
+using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 
 namespace Core.Service.FileZipperService.Zippers
 {
@@ -10,15 +13,34 @@ namespace Core.Service.FileZipperService.Zippers
     {
         public void ZipProject(string path)
         {
+            string destinyFileName = string.Format(@"{0}\{1}.zip", ConfigurationManager.AppSettings["DESTINY_FOLDER"], Path.GetFileName(path));
             using (ZipFile zip = new ZipFile())
             {
                 zip.AddDirectory(string.Format(@"{0}", path));
 
                 zip.Comment = "This zip was created at " + System.DateTime.Now.ToString("G");
 
-                zip.MaxOutputSegmentSize = Constants.ONE_GIGABYTE_SIZE * int.Parse(ConfigurationManager.AppSettings["SEGMENT_SIZE"]);
+                zip.MaxOutputSegmentSize = int.MaxValue / 2;
+
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
 
                 zip.Save(string.Format(@"{0}\{1}.zip", ConfigurationManager.AppSettings["DESTINY_FOLDER"], Path.GetFileName(path)));
+
+                if (zip.NumberOfSegmentsForMostRecentSave > 1)
+                {
+                    List<string> filesToDelete = Directory.GetFiles(string.Format(@"{0}", ConfigurationManager.AppSettings["DESTINY_FOLDER"])).ToList();
+
+                    filesToDelete.ForEach(x =>
+                    {
+                        if (x.Contains(Path.GetFileName(path)))
+                        {
+
+                            File.Delete(x);
+                        };
+                    });
+
+                    throw new Exception("Project will need more than one splitted file");
+                }
             }
         }
     }
