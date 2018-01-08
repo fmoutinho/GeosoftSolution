5bf82b01f7d55914813cc881d8304c110f888f36
fmoutinho <fernandom@gmail.com>
2018-01-08 15:19:13 -0200

    FolderRenamer - Corrige tratamentos Cria SQL com a base

---
+++ Core/SQL/Alimenta Expression.sql
@@ -0,0 +1,297 @@
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '007  Argentina' , @Replacement = 'Argentina'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'South_America' , @Replacement = 'SAM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'South America' , @Replacement = 'SAM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'C 01 AA  South America' , @Replacement = 'SAM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DCFS_PreIMPcleanUp_11Aug06' , @Replacement = 'dcfs'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Europe' , @Replacement = 'EUR'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'North America' , @Replacement = 'NAM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'North_America' , @Replacement = 'NAM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Agreements- Billiton London office X Drive Back-Up per SB' , @Replacement = 'Agreements'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Documentation Re - Programme' , @Replacement = 'Documentation'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Tape Data Santiago' , @Replacement = 'Santiago'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01  Administration' , @Replacement = 'admin'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'T     REPORTS (choose from below)' , @Replacement = 'report'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '10 Monthlies All  of Cu documents' , @Replacement = 'Monthlies All of Cu documents'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '05 By Project' , @Replacement = 'By Project'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '02 Province Stage' , @Replacement = 'Province Stage'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01 Andean Region Cerro Nevado, Argentina' , @Replacement = 'Andean Region Cerro Nevado, Argentina'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Internal Procedures' , @Replacement = 'Procedures'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01 STS' , @Replacement = 'STS'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Tech_PER - MOVED TO SSMPER-FIL01' , @Replacement = 'SSMPER-FIL01'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Historical_Archives' , @Replacement = 'Historial'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Application' , @Replacement = 'app'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Eric''s Old Data' , @Replacement = 'Eric''s Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Documents' , @Replacement = 'Doc'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '_zip' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Project Generation' , @Replacement = 'proj_gen'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Administration' , @Replacement = 'admin'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'South_Jwaneng_Nov_2004_reprocessing_AGG_data' , @Replacement = 'South_Jwaneng_AGG_data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'ext_hdd_data_BF' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Commodities June 2009' , @Replacement = 'Commodities'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'm-svr6-fri-12-28-07 session 2' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Nyange-Songa\Songa\Songa ArcGIS' , @Replacement = 'Songa ArcGIS'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'jnbhol-file01' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Afr_HD2_DRC' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'LEVI DATA\LEVI LAPTOP\LAPTOP_LEVI_20080227' , @Replacement = 'LEVI DATA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Zak_Back_up_Feb08\Data\DRC Data\DRC DATA UPDATED' , @Replacement = 'DRC Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'LEVI DATA\DR CONGO_From LEVI' , @Replacement = 'LEVI DATA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Zak_Back_up_Feb08\Data\DRC Data\DRC_other_Data\drc' , @Replacement = 'DRC Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Mwana Results\Mwana Picking & Minchem Results\Mwana Ilmenite Fingerprinting' , @Replacement = 'Mwana and Minchem Results'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Gravity Capital Priority areas August 2006' , @Replacement = 'Gravity Capital Priority areas'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRC_Gravity_Data_27 July 2006' , @Replacement = 'DRC Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'yvrame-svr6' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Central_and_Eastern_Asia_Porphyry_Belt_Study' , @Replacement = 'CEAP'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Africa' , @Replacement = 'AFR'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Asia' , @Replacement = 'ASA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Data_Copied from Brisbane Office in March 2005' , @Replacement = 'Brisbane'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'A 01 AA   admin' , @Replacement = 'admin'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'X_Drive' , @Replacement = 'D_X'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'GIS_OriginalData' , @Replacement = 'GIS_Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '03  User Suppor' , @Replacement = 'User Support'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '3.04  ASA' , @Replacement = 'ASA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '3.4.04  Pakistan' , @Replacement = 'Pakistan'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Tchibanga_Base_data_mxd_data.gdb' , @Replacement = 'Tchibanga_Base_data_mxd_data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '011 Lukula and Kasaka' , @Replacement = 'Lukula and Kasaka'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Sept14_Applications' , @Replacement = 'app'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'D 01 AA  South ASA' , @Replacement = 'South ASA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'D 085 AA  South ASA' , @Replacement = 'South ASA'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRIVE_F - iomega' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'NAEops (2.8 GB)' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Historial_data_for_indexing' , @Replacement = 'Historial' 
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'WBKASAI_Geophysical_Data_20070826' , @Replacement = 'Geophysical_Data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '4 DRC' , @Replacement = 'DRC'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '4 Other' , @Replacement = 'Other'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '02 Zambia' , @Replacement = 'Zambia'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '09  Geology' , @Replacement = 'Geology'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '15  Technical Due Diligence' , @Replacement = 'Technical Due Diligence'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Tchibanga_Data-2009.02.02' , @Replacement = 'Tchibanga_Data'
+
+ exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '1Russell (19-20 March 2009)' , @Replacement = 'Russell'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '003 Kaoma_LPL389_BDZ' , @Replacement = 'Kaoma_LPL389_BDZ'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '004 Kasaka_LPL375_BDZ' , @Replacement = 'Kasaka_LPL375_BDZ'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '02 Kasanka Reaplication Mining Right' , @Replacement = 'Kasanka Reaplication Mining Right'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Mukuba data Jun 2010' , @Replacement = 'Mukuba data'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Internal\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Porphyry\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '0065 Development' , @Replacement = 'Development'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '0067 Research' , @Replacement = 'Research'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'K Geophysics' , @Replacement = 'Geophysics'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Fourth Hearing_4Oct05' , @Replacement = 'Fourth Hearing'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '18  Practice Network - Geology' , @Replacement = 'Practice Network - Geology'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Geophysics\Ground_Geophysics\Kapota_Ground_Geophysics' , @Replacement = 'Geophysics'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'MISC_Micro Diamond Sampling Details_PawlitschekM_0411' , @Replacement = 'MISC_Micro Diamond Sampling Details_PawlitschekM'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'D 084 AA  South ASA - India Exploration' , @Replacement = 'South ASA - India Exploration'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01  General, Reconnaissance' , @Replacement = 'General, Reconnaissance'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'G 01 AA  AFR, Mediterranean' , @Replacement = 'AFR, Mediterranean'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'J Geology' , @Replacement = 'Geology'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRC REPORTS ''06' , @Replacement = 'DRC REPORTS'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Kapota Camp_HSEC Documentation_September 2006' , @Replacement = 'Kapota Camp_HSEC Documentation'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'A Correspondence' , @Replacement = 'Correspondence'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Reaplication Mines Act 2008' , @Replacement = 'Reaplication Mines'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '007 Kalengwa_apps' , @Replacement = 'Kalengwa_apps'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'JWA Lunga 2008' , @Replacement = 'JWA Lunga'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'My Doc 2010\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRC_GeoDatabases_20081119' , @Replacement = 'DRC_GeoDatabases'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'For ZAK' , @Replacement = 'ZAK'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRC REPORTS'' 07' , @Replacement = 'DRC REPORTS'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Kaponda Documentation_11 07' , @Replacement = 'Kaponda Documentation'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Kaponda Camp_HSEC Documentation_October 2006' , @Replacement = 'Kaponda Camp_HSEC Documentation'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Doc\Doc 2010' , @Replacement = 'Doc'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'StopWorkDay Documentation_11 10 2006_DRC_VanStadenR' , @Replacement = 'StopWorkDay Documentation DRC_VanStadenR'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'DRC HSEC_Handover docs_02 2008_VanStadenR' , @Replacement = 'DRC HSEC_Handover docs_VanStadenR'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Draft Concept Phase E&C Submissions 29 June 2005' , @Replacement = 'Draft Concept Phase E&C Submissions'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Saturday 30 April 2005' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'RE Backup_24 September 2012' , @Replacement = 'RE Backup'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '2- Kalengwa' , @Replacement = 'Kalengwa'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '09_Title Strategies' , @Replacement = 'Title Strategies'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01_Commerial' , @Replacement = 'Commerial'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '002 Kasempa_LPL275_Lufillian' , @Replacement = 'Kasempa_LPL275_Lufillian'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '2009_2010 Western Zambia  Falcon Survey' , @Replacement = 'Western Zambia  Falcon Survey'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '2$' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'StopworkDay_kapota_10072007' , @Replacement = 'StopworkDay_kapota'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'StopWorkDay Documentation_10 07 2007_DRC_VanStadenR' , @Replacement = 'StopWorkDay Documentation DRC_VanStadenR'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '(Mukuba)' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '03 Mulongo Reaplication Mining Right' , @Replacement = 'Mulongo Reaplication Mining Right'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '005 Mulongo_LPL336_BDZ' , @Replacement = 'Mulongo_LPL336_BDZ'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '01 Final Applications Lodged by EvansSept2009' , @Replacement = 'Final Applications Lodged by Evans'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '09 Tenements' , @Replacement = 'Tenements'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'BHPB Kalengwa Applications_Submitted to Cadastre_KancheE_20090914' , @Replacement = 'BHPB Kalengwa Applications_Submitted to Cadastre_KancheE'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Second Hearing_21July05' , @Replacement = 'Second Hearing'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Third Hearing_16Sep05' , @Replacement = 'Third Hearing'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Afr_SLiebenberg_Zambia\Zambia' , @Replacement = 'Afr_SLiebenberg_Zambia'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Mike_Grimleys_computer files_jan2006' , @Replacement = 'Mike_Grimleys_computer files'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Mag 14-3 samples MinServ and Crown, April 2005' , @Replacement = 'Mag 14-3 samples MinServ and Crown'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'AustASA PA compilation of old reports' , @Replacement = 'AustASA PA old reports'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Other company reports to be georeferenced' , @Replacement = 'reports to be georeferenced'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '\Historial\dcfscopy\Brisbane\dcfs' , @Replacement = '\Historial\Brisbane'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'mohal_India my docs' , @Replacement = 'mohal_India'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Ethiopia\Ethiopian Flood Basalt Project\Doc\Ethiopian Flood Basalt Project\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '! Reports' , @Replacement = 'Reports'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '\Projects\' , @Replacement = '\Proj\'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Drilling\DDrilling' , @Replacement = 'Drilling'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Logon\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'VBScripts\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Kapota Documentation_11 07' , @Replacement = 'Kapota Documentation'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'public' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '00\' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '2007WorkingProjData' , @Replacement = 'WorkingProjData'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '00_TECHNICAL' , @Replacement = 'TECHNICAL'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'ProjectoaltoCuilo' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '2_Drill' , @Replacement = 'Drill'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '1_Current' , @Replacement = 'Current'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '4_WomsSouthCentraline' , @Replacement = 'WomsSouthCentraline'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '02 HSEC_Angola' , @Replacement = 'HSEC_Angola'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '02.05.03_' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '05_' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = '_25thJuly07' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'KapotaDrilling2008' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = ' 2002-2003' , @Replacement = ''
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Kapota Main RC Drilling Database2006' , @Replacement = 'Kapota Main RC Drilling Database'
+
+exec [dbo].[UP_Insert_Expression_To_Replace] @Expression = 'Sheba Resources Renewal reports Dec 2007' , @Replacement = 'Sheba Resources Renewal reports'
---
+++ Core/SQL/IX_Expression_Description_Replacement.sql
@@ -0,0 +1,4 @@
+
+CREATE UNIQUE INDEX IX_Expression_Description_Replacement
+   ON dbo.Expression([Description],[Replacement]);   
+GO  
